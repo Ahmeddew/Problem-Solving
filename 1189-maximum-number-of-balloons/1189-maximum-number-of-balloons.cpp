05fe1932e422fb5b59141216632b797cab84faf8
Ahmed Hany <ah12122005@gmail.com>
2026-05-24 22:22:18 +0300

    Time: 0 ms (100%), Space: 8.9 MB (59.97%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        map<char,int>mp ;
-        for(int i=0; i< text.size(); i++){
-            mp[text[i]]++;
+        map<char,int> mp;
+
+        for (char c : text) {
+            mp[c]++;
         }
-    int ans1=min({mp['a'],mp['b'],mp['n']});
-    int ans2=min({mp['l'],mp['o']})/2;
-             
-      return min(ans1,ans2) == 0 ?0 : min(ans1,ans2);  
-       
+
+        return min({
+            mp['b'],
+            mp['a'],
+            mp['l'] / 2,
+            mp['o'] / 2,
+            mp['n']
+        });
     }
 };
