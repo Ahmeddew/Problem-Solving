e331f5da89ef04d0e53f9c90a9ad52cffd5d93dc
Ahmed Hany <ah12122005@gmail.com>
2026-05-24 04:19:48 +0300

    Time: 0 ms (100%), Space: 35.7 MB (53.1%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,21 +1,16 @@
-// Time Complexity : O(n)
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        // Initialize rightSum to store the sum of all the numbers strictly to the index's right...
-        int rightSum = accumulate(nums.begin(), nums.end(), 0);
-        // Initialize leftSum to store the sum of all the numbers strictly to the index's left...
-        int leftSum = 0;
-        // Traverse all elements through the loop...
-        for (int idx = 0; idx < nums.size(); idx++) {
-            // subtract current elements with from rightSum...
-            rightSum -= nums[idx];
-            // If the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right...
-            if (leftSum == rightSum)
-                return idx;     // Return the pivot index...
-            // add current elements with leftSum...
-            leftSum += nums[idx];
+        int sum=0;
+        for(auto i:nums)sum+=i; 
+        int leftsum= 0;
+         for(int i=0; i< nums.size() ;i++){
+            sum-= nums[i];
+            if (sum == leftsum ){
+                   return i ;
+            }
+            leftsum+=nums[i];
         }
-        return -1;      // If there is no index that satisfies the conditions in the problem statement...
+        return -1;
     }
 };
