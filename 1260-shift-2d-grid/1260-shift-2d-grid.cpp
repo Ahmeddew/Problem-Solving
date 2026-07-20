73878852465e30f1bcd9dfecf6ebde00466bdc3a
Ahmed Hany <ah12122005@gmail.com>
2026-07-20 18:11:29 +0300

    Time: 0 ms (100%), Space: 18.1 MB (91.78%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> shiftGrid(vector<vector<int>>& v, int k) {
-         int n = v.size(), m = v[0].size();
-        vector<vector<int>> ans = v;
-        while (k--){
-    ans[0][0] = v[n - 1][m - 1];
-    for (int i = 0; i < v.size() ; ++i) {
-        for (int j = 0; j <v[i].size() ; ++j) {
-            if (i == n - 1 && j == m - 1)
-                continue;
-                if ( j == m-1 ){
-                    ans[i+1][0]=v[i][j];
-                    continue;
-                }
-                ans[i][j+1]=v[i][j];
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
 
+        vector<vector<int>> ans(n, vector<int>(m));
+
+        k %= (n * m); 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int idx = i * m + j;
+                int newIdx = (idx + k) % (n * m);
+
+                int newRow = newIdx / m;
+                int newCol = newIdx % m;
+
+                ans[newRow][newCol] = grid[i][j];
+            }
         }
-    }
- 
-        v = ans;
-        }
+
         return ans;
     }
 };
