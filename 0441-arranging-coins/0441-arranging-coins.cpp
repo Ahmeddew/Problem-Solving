e1da4e3c3c55b81dd4e090304dde2d3923358e2f
Ahmed Hany <ah12122005@gmail.com>
2026-07-31 07:39:56 +0300

    Time: 0 ms (100%), Space: 8.6 MB (96.9%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long l = 0, r = n;
+        long long ans = 0;
+
+        while (l <= r) {
+            long long mid = l + (r - l) / 2;
+            long long coins = mid * (mid + 1) / 2;
+
+            if (coins <= n) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
