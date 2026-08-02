7cd69f1deb3f36f6dc5580144b3d01b37ab5b762
Ahmed Hany <ah12122005@gmail.com>
2026-08-02 23:48:39 +0300

    Time: 3 ms (37.46%), Space: 41.6 MB (38.87%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int ProductWithoutZeros= 1,zeros=0;
+        for(int i : nums){
+            if (i == 0)zeros++;
+              else {
+                ProductWithoutZeros*=i;
+              }
+        } 
+          vector<int>ans;
+        for(int i :nums) {
+        if ( !zeros ){
+            ans.push_back(ProductWithoutZeros/i);
+        }else {
+             if (i != 0 ){
+                ans.push_back(0);
+             }else {
+                 if (i == 0 &&zeros == 1 ){
+                    ans.push_back(ProductWithoutZeros);
+                 }else {
+                    ans.push_back(0);
+                 }
+             }
+        }
+        }
+        return ans;
+    }
+};
