6af25d5ffa3123f218840d29e75601edbe3be8d3
Ahmed Hany <ah12122005@gmail.com>
2026-08-05 15:02:03 +0300

    Time: 1 ms (96.44%), Space: 47.1 MB (82.56%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] Intersect(int[] nums1, int[] nums2) {
+         Dictionary<int, int> mp = new Dictionary<int, int>();
+            List<int> ans = new();
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                if (! mp.ContainsKey(nums1[i])) mp[nums1[i]]=0;
+
+                mp[nums1[i]]++;
+            }
+            foreach(int i in nums2)
+            {
+                if (mp.ContainsKey(i) && mp[i] > 0 )
+                {
+                    ans.Add(i);
+                    mp[i]--;
+                    
+                }
+            }
+
+            return ans.ToArray();
+    }
+}
