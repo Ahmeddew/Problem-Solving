79e66917e68f20a6faecdefdb5560291ec2c93c2
Ahmed Hany <ah12122005@gmail.com>
2026-08-05 19:03:03 +0300

    Time: 7 ms (72.13%), Space: 71.8 MB (45.36%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cs
+++ 0705-design-hashset/0705-design-hashset.cs
@@ -1,29 +1,26 @@
- public class MyHashSet {
-
-        public MyHashSet() {
-        
-        }
-
-         List<int> st = new();
-        public void Add(int key)
-        {
-            if (!Contains(key))
-            {
-                st.Add(key);
-            }
-        }
-    
-        public void Remove(int key) {
-            if (Contains(key))
-            {
-                st.Remove(key);
-            }
-        }
-    
-        public bool Contains(int key) {
-             foreach(int i in st){
-                if (i == key )return true;
-              }
-            return false ;
-        }
+public class MyHashSet {
+    private bool[] numSet;
+    public MyHashSet() {
+        numSet = new bool[1000001];
     }
+    
+    public void Add(int key) {
+        numSet[key] = true;
+    }
+    
+    public void Remove(int key) {
+        numSet[key] = false;
+    }
+    
+    public bool Contains(int key) {
+        return numSet[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * bool param_3 = obj.Contains(key);
+ */
