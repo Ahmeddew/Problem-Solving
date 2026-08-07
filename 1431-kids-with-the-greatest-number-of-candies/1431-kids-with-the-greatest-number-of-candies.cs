8d7e5f2015a9612445383161c99dab6b66d2c4bc
Ahmed Hany <ah12122005@gmail.com>
2026-08-07 12:52:46 +0300

    Time: 0 ms (100%), Space: 49.5 MB (6.41%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
@@ -2,17 +2,16 @@ public class Solution {
       public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
       {
           List<bool> ans = new ();
-          int[] arr=new int[candies.Length];
-          int mx=int.MinValue;
+           int mx=int.MinValue;
           for (int i = 0; i < candies.Length; i++)
           {
               mx = Math.Max(mx, candies[i]);
-              arr[i] = candies[i] + extraCandies;
+              
           }
 
-          foreach (int i in arr)
+          foreach (int i in candies)
           {
-              if (i >= mx)
+              if (i+extraCandies >= mx)
               {
                   ans.Add(true);
               }
