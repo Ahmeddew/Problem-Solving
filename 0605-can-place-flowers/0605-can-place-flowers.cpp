a93957843e2a9c240c33220db6d5ce856dee0ae7
Ahmed Hany <ah12122005@gmail.com>
2026-08-07 17:32:38 +0300

    Time: 0 ms (100%), Space: 24 MB (77.6%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -20,7 +20,7 @@ public:
             if ( i>0 && i<arr.size()-1 && arr[i] ==0 
             &&arr[i-1]==0 && arr[i+1] ==0){
                   n--;
-                  arr[i]=1;
+                   
                  i++;
             }
              
