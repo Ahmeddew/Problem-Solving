f1f5221ddaf29cba17fbd31844422337df5ee84b
Ahmed Hany <ah12122005@gmail.com>
2026-08-13 19:48:37 +0300

    Time: 5 ms (38.48%), Space: 22.1 MB (55.3%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-int Palindrome(string s){
-    int l=0,r=s.size()-1;
-    while (l<r){
-        if (s[l] != s[r])
-        {
-        return false ;
-            }
-        l++,r--;
-    }
-    return true ;
-}
     bool validPalindrome(string s) {
-         // abcxacba   
-        int l=0,r=s.size()-1;
-        string t=s,w=s;
-    int x=0,y=0;
-    while (l<r){
-        if (s[l] != s[r])
-        {
-        t.erase(l, 1);
-        w.erase(r, 1);
-        break;
-        cout<<w<<endl;
-        cout<<t<<endl;
-         }
-        l++,r--;
+        int left=0;
+        int right=s.size()-1;
+
+        while(left<right){
+            if(s[left]!=s[right]){
+                return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
+
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
-     return Palindrome(t)||Palindrome(w);
+
+private:
+    bool isPalindrome(string &s,int left,int right){
+        
+
+        while(left<right){
+            if(s[left]!=s[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
+    
 };
