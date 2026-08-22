fad10f844394d4b0f16a0c84f831d9863f4141ad
Ahmed Hany <ah12122005@gmail.com>
2026-08-23 00:20:12 +0300

    Time: 0 ms (100%), Space: 8.9 MB (82.4%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -8,14 +8,11 @@ public:
         int zeros=s.size()-ones;
          // 11101
         string ans=""; 
-          if (ones>1 ){
+        
              for(int i= 0 ;i<ones-1; i++)ans+="1";
              for(int i=0; i<zeros;i++)ans+="0";
              ans+="1";
-          }else {
-             for(int i=0; i<zeros;i++)ans+="0";
-             ans+="1";
-          }
+           
           return ans;
     }
 };
