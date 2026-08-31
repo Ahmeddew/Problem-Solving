7bd8938e20ca132e3dfa4ad5fcd134ba389faf66
Ahmed Hany <ah12122005@gmail.com>
2026-08-31 04:38:17 +0300

    Time: 0 ms (100%), Space: 16.7 MB (77.24%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& arr) {
-        int n= arr.size();
-         vector<vector<int>>ans;
-        map<int,vector<int>>mp;
-        for(int i=0;i <n;i++){
-            mp[arr[i]].push_back(i);
-        }
-        for(auto p:mp){
-            int num=p.second.size();
-            vector<int>&t=p.second;
-             vector<int>r;
-               for(int i=0 ; i<num ;i++){
-                        r.push_back(t[i]);
-                       if (r.size() == p.first){
-                        ans.push_back(r);r.clear();
-                       }
-               }
+        vector<vector<int>> ans;
+        unordered_map<int, vector<int>> mp;
 
+        for (int i = 0; i < arr.size(); i++) {
+            int groupSize = arr[i];
+
+            mp[groupSize].push_back(i);
+
+            if (mp[groupSize].size() == groupSize) {
+                ans.push_back(mp[groupSize]);
+                mp[groupSize].clear();
+            }
         }
+
         return ans;
     }
 };
