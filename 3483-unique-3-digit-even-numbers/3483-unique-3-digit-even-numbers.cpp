c8bb7c4e5a38ce1740cd7b6836c0e648e789a770
Ahmed Hany <ah12122005@gmail.com>
2026-09-13 17:53:08 +0300

    Time: 4 ms (59.31%), Space: 28.7 MB (91.92%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
-void backtrack(vector<int>&digits,string curr,set<int>&st,vector<bool>&vis){
- 
-     if (curr.size() == 3){
-         if (stoi(curr)%2 ==0 )st.insert(stoi(curr));
-            
-         return;
-     }
-    //choose
-    for (int i = 0; i <digits.size() ; ++i) {
-        
-         if (vis[i])continue;
-         if (curr.empty() && digits[i] == 0)continue;
+    void backtrack(vector<int>& digits, string& curr,
+                   vector<bool>& vis, int& ans)
+    {
+        if (curr.size() == 3)
+        {
+            if ((curr.back() - '0') % 2 == 0)
+                ans++;
 
-             vis[i] =true;
-             curr.push_back(digits[i]+'0');
-             // recurse 
-             backtrack(digits,curr,st,vis);
-              // undo 
-             curr.pop_back();
-              vis[i] =false;
-           
+            return;
+        }
+
+        for (int i = 0; i < digits.size(); i++)
+        {
+            if (vis[i])
+                continue;
+
+            // Skip duplicate choices at the same level
+            if (i > 0 && digits[i] == digits[i - 1] && !vis[i - 1])
+                continue;
+
+            // No leading zero
+            if (curr.empty() && digits[i] == 0)
+                continue;
+
+            vis[i] = true;
+            curr.push_back(digits[i] + '0');
+
+            backtrack(digits, curr, vis, ans);
+
+            curr.pop_back();
+            vis[i] = false;
+        }
     }
-}
-    int totalNumbers(vector<int>& digits) {
-     vector<bool>vis(digits.size(),false);
-      set<int>st;
 
-   backtrack(digits,"",st,vis);
-   return st.size();
+    int totalNumbers(vector<int>& digits)
+    {
+        sort(digits.begin(), digits.end());
+
+        vector<bool> vis(digits.size(), false);
+        string curr;
+        int ans = 0;
+
+        backtrack(digits, curr, vis, ans);
+
+        return ans;
     }
 };
