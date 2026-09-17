14f715517de885e67006fc26e94548d491a402e0
Ahmed Hany <ah12122005@gmail.com>
2026-09-17 07:54:41 +0300

    Time: 5 ms (28.83%), Space: 11.9 MB (49.57%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-         map<char,int>mp1;
-         map<char,int>mp2;
+         unordered_map<char,int>mp1;
+         unordered_map<char,int>mp2;
          for(char c :magazine)mp2[c]++;
          for(char c :ransomNote)mp1[c]++;
 
