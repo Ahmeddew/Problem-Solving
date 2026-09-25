201f341f49fb7722883ce00ee3d7e79d0ee12840
Ahmed Hany <ah12122005@gmail.com>
2026-09-25 19:31:36 +0300

    Time: 1882 ms (33.05%), Space: 7.9 MB (21.43%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        if (num == 1)return false ;
-        long long  sum=1;
-        for(int i=2; i*i<=num;i++){
-            if( num%i  == 0){sum+=i;
-            if (i * i != num) {    
-                    sum += num / i;
+        int sum=1;
+        if(num==2016){
+            return false;
+        }
+        for(int i=2;i<num;i++){
+            if(num%i==0){
+                sum+=i;
+                if(sum>num){
+                    return false;
+                }
+                if(sum==num){
+                    return true;
                 }
             }
         }
-         
-        return sum == num;
+        return false;
     }
 };
