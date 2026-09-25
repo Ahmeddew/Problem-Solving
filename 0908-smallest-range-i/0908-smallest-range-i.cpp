38156adc1c8648d263cbef0d5a7333d75f3e92b2
Ahmed Hany <ah12122005@gmail.com>
2026-09-25 23:30:36 +0300

    Time: 0 ms (100%), Space: 19.3 MB (58.46%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.cpp
+++ 0908-smallest-range-i/0908-smallest-range-i.cpp
@@ -4,23 +4,7 @@ public:
         if (nums.size() == 1)return 0;
       int mn= *min_element(nums.begin(),nums.end());
       int mx= *max_element(nums.begin(),nums.end());
-      int  ans=INT_MAX;
-       vector<int>a;
-       vector<int>b;
-       for(int i=0 ;i<=k ;i++){
-        a.push_back(mn+i);
-       }
-         for(int i=0 ;i<=k ;i++){
-        b.push_back(mx-i);
-       }
-       for(int i=0; i< b.size();i++){
-            int r= b[i];
-            for(int j =0; j< a.size() ;j++){
-                ans= min(ans,r-a[j]);
-                if (ans== 0 )return ans;
-            }
-       }
-
-         return ans;
+       
+         return max(0,(mx-k)-(mn+k));
     } 
 };
